68e3695148ed3a8a6bf0ea2265430d06b5d0f572
exosignals <exosignals@gmail.com>
2025-08-20 18:56:01 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1945,7 +1945,7 @@ def main():
     app.add_handler(CommandHandler("dar", dar))
     app.add_handler(CallbackQueryHandler(transfer_callback, pattern=r'^(confirm_dar_|cancel_dar_)'))
     app.add_handler(CommandHandler("abandonar", abandonar))
-    app.add_handler(CallbackQueryHandler(callback_abandonar, pattern=r'^confirm_abandonar_|^cancel_abandonar$'))
+    app.add_handler(CallbackQueryHandler(callback_abandonar, pattern=r'^confirm_abandonar_|^cancel_abandonar_'))
     app.add_handler(CommandHandler("consumir", consumir))
     app.add_handler(CallbackQueryHandler(callback_consumir, pattern=r'^confirm_consumir_|^cancel_consumir_'))
     app.add_handler(CommandHandler("dano", dano))
